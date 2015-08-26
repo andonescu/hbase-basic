27c9dc98c39c5f12fc5a42014bd0f25eb13c93df
Ionut Andonescu <ionut.andonescu@pure360.com>
2015-08-26 15:22:10 +0300

    init project

---
+++ src/main/java/basic/AddData.java
@@ -0,0 +1,26 @@
+package basic;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class AddData {
+
+    public static void main(String[] args) throws IOException {
+        Configuration conf = HBaseConfiguration.create();
+        HTable table = new HTable(conf, "ando-table");
+
+        Put put = new Put(Bytes.toBytes("row1"));
+        put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("value1"));
+        put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("value2"));
+        put.add(Bytes.toBytes("colfam2"), Bytes.toBytes("qual1"), Bytes.toBytes("value3"));
+
+        table.put(put);
+        table.flushCommits();
+        table.close();
+    }
+}

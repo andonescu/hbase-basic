e1df0de9dacdd6afafb7c016c1d3a1530abad36d
Ionut Andonescu <ionut.andonescu@pure360.com>
2015-08-26 15:39:23 +0300

    removed some deprecated code

--- src/main/java/basic/AddData.java
+++ src/main/java/basic/AddData.java
@@ -17,9 +17,9 @@ public class AddData {
         HTable table = new HTable(conf, "ando-table");
 
         Put put = new Put(Bytes.toBytes("row1"));
-        put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("value1"));
-        put.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("value2"));
-        put.add(Bytes.toBytes("colfam2"), Bytes.toBytes("qual1"), Bytes.toBytes("value3"));
+        put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("value1"));
+        put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"), Bytes.toBytes("value2"));
+        put.addColumn(Bytes.toBytes("colfam2"), Bytes.toBytes("qual1"), Bytes.toBytes("value3"));
 
         table.put(put);
         table.flushCommits();

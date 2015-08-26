bc5b0bf283501f816696ca9600e9b04c3e5284b7
Ionut Andonescu <ionut.andonescu@pure360.com>
2015-08-26 16:38:47 +0300

    inserting two column family names

--- src/main/java/basic/AddData.java
+++ src/main/java/basic/AddData.java
@@ -5,8 +5,11 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class AddData {
 
@@ -16,10 +19,28 @@ public class AddData {
         conf.set("hbase.zookeeper.quorum", "172.30.140.203");
         conf.set("hbase.master", "172.30.140.203");
 
-        creatTable(conf);
+        TableName tableName = creatTable(conf);
+        addData(conf, tableName);
     }
 
-    public static void creatTable(Configuration conf)
+    private static void addData(Configuration conf, TableName tableName) throws IOException {
+
+        String rowKey = UUID.randomUUID().toString();
+
+        Table table = ConnectionFactory
+                .createConnection(conf)
+                .getTable(tableName);
+
+
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name"), Bytes.toBytes("Ionut Ando"));
+        put.addColumn(Bytes.toBytes("contactinfo"), Bytes.toBytes(""), Bytes.toBytes("ando@ando.ro"));
+        table.put(put);
+        System.out.println("insert recored " + rowKey + " to table "
+                + tableName + " ok.");
+    }
+
+    public static TableName creatTable(Configuration conf)
             throws Exception {
         Admin admin = ConnectionFactory.createConnection(conf).getAdmin();
         TableName tableName = TableName.valueOf("ando-test");
@@ -36,5 +57,9 @@ public class AddData {
 
             System.out.println("create table " + tableName + " ok.");
         }
+
+        admin.close();
+
+        return tableName;
     }
 }

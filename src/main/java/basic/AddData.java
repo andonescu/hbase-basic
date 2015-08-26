1d4305ed6a9ff19ad678911b9557b7e0c3ac35b0
Ionut Andonescu <ionut.andonescu@pure360.com>
2015-08-26 17:11:28 +0300

    phonebook-solution-1

--- src/main/java/basic/AddData.java
+++ src/main/java/basic/AddData.java
@@ -1,11 +1,13 @@
 package basic;
 
+import com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 public class AddData {
@@ -17,7 +19,13 @@ public class AddData {
         conf.set("hbase.master", "172.30.140.203");
 
         TableName tableName = creatTable(conf);
-        addData(conf, tableName);
+        addData(conf, tableName,
+                Lists.newArrayList(
+                        new Phonebook("Ionut", "Ando", "+4076666666"),
+                        new Phonebook("Dan", "Rares", "+07662738733"),
+                        new Phonebook("Mircea", "Popa", "+07662738733")
+                )
+        );
 
         getAllRecord(conf, tableName);
 
@@ -25,24 +33,22 @@ public class AddData {
 
     }
 
-    public static void getAllRecord (Configuration conf, TableName tableName) {
-        try{
+    public static void getAllRecord(Configuration conf, TableName tableName) {
+        try {
             Table table = ConnectionFactory
                     .createConnection(conf)
                     .getTable(tableName);
 
             Scan s = new Scan();
             ResultScanner ss = table.getScanner(s);
-            for(Result r:ss){
-                for(KeyValue kv : r.raw()){
+            for (Result r : ss) {
+                for (KeyValue kv : r.raw()) {
                     System.out.print(new String(kv.getRow()) + " ");
-                    System.out.print(new String(kv.getFamily()) + ":");
-                    System.out.print(new String(kv.getQualifier()) + " ");
                     System.out.print(kv.getTimestamp() + " ");
-                    System.out.println(new String(kv.getValue()));
+                    System.out.println();
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -61,7 +67,7 @@ public class AddData {
         admin.close();
     }
 
-    private static void addData(Configuration conf, TableName tableName) throws IOException {
+    private static void addData(Configuration conf, TableName tableName, List<Phonebook> phonebook) throws IOException {
 
         String rowKey = UUID.randomUUID().toString();
 
@@ -69,13 +75,17 @@ public class AddData {
                 .createConnection(conf)
                 .getTable(tableName);
 
+        for (Phonebook contact : phonebook) {
 
-        Put put = new Put(Bytes.toBytes(rowKey));
-        put.addColumn(Bytes.toBytes("personal"), Bytes.toBytes("name "), Bytes.toBytes("Ionut Ando"));
-        put.addColumn(Bytes.toBytes("contactinfo"), Bytes.toBytes(""), Bytes.toBytes("ando@ando.ro"));
-        table.put(put);
-        System.out.println("insert recored " + rowKey + " to table "
-                + tableName + " ok.");
+            Put put = new Put(
+                    Bytes.toBytes(String.format("%s-%s-%s", contact.getFirstname(), contact.getLastname(), contact.getPhone()))
+            );
+
+            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes(""), Bytes.toBytes(""));
+            table.put(put);
+            System.out.println("insert recored " + rowKey + " to table " + tableName + " ok.");
+
+        }
 
         table.close();
     }
@@ -83,16 +93,16 @@ public class AddData {
     public static TableName creatTable(Configuration conf)
             throws Exception {
         Admin admin = ConnectionFactory.createConnection(conf).getAdmin();
-        TableName tableName = TableName.valueOf("ando-test");
+        TableName tableName = TableName.valueOf("phonebook-ando-op1");
 
 
         if (admin.tableExists(tableName)) {
             System.out.println("table already exists!");
         } else {
             HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
-            tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-            tableDescriptor.addFamily(new HColumnDescriptor("contactinfo"));
-            tableDescriptor.addFamily(new HColumnDescriptor("creditcard"));
+            tableDescriptor.addFamily(new HColumnDescriptor("data"));
+
+
             admin.createTable(tableDescriptor);
 
             System.out.println("create table " + tableName + " ok.");
@@ -101,5 +111,50 @@ public class AddData {
         admin.close();
 
         return tableName;
+    }
+}
+
+class Phonebook {
+    private String firstname;
+    private String lastname;
+    private String phone;
+
+    public Phonebook(String firstname, String lastname, String phone) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Phonebook{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
